e39e5cf8cec94ecf581571337664cdc8cd565757
Evgeniya Stambolova <70650990+evgeniya-stambolova@users.noreply.github.com>
2020-10-03 13:09:57 +0300

    UPR

---
+++ Advanced/SumBytes.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class SumBytes {
+    public static void main(String[] args) throws IOException {
+
+        Path path = Path.of ( "C:\\Users\\Evgeniq.000\\Desktop\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\input.txt" );
+
+        long sum = 0;
+
+        byte[] symbol = Files.readAllBytes ( path );
+        for (byte bytes: symbol){
+            if(bytes ==10 || bytes==13){
+                continue;
+            }
+            sum+=bytes;
+        }
+
+        System.out.println (sum);
+    }
+}
