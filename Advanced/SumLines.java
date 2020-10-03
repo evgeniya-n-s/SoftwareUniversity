ee0057319a61d497ff49702dcf91afb2a239c20a
Evgeniya Stambolova <70650990+evgeniya-stambolova@users.noreply.github.com>
2020-10-03 13:10:20 +0300

    UPR

---
+++ Advanced/SumLines.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class SumLines {
+    public static void main(String[] args) throws IOException {
+
+        Path path = Path.of("C:\\Users\\Evgeniq.000\\Desktop\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\input.txt");
+
+        List<String> lines = Files.readAllLines (path);
+
+        lines.stream ().map ( String::toCharArray ).forEach ( e->{
+             int sum = 0;
+             for (char symbol:e){
+                 sum+=symbol;
+             }
+            System.out.println (sum);
+        } );
+    }
+}
