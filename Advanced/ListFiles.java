0e6a49f2d40b545da1564d733f9b450e582467f2
Evgeniya Stambolova <70650990+evgeniya-stambolova@users.noreply.github.com>
2020-10-05 19:43:40 +0300

    Update ListFiles.java

--- Advanced/ListFiles.java
+++ Advanced/ListFiles.java
@@ -2,7 +2,7 @@ import java.io.File;
 
 public class ListFiles {
     public static void main(String[] args) {
-        File file = new File ("C:\\Users\\Evgeniq.000\\Desktop\\04. Java-Advanced-Files-and-Streams-Lab-Resources");
+        File file = new File ("C:\\Users\\000\\Desktop\\04. Java-Advanced-Files-and-Streams-Lab-Resources");
 
         File[] files = file.listFiles ();
         for (File f : files) {
