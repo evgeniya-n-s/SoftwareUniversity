6b7fe158adfeea1bea332efddae188edfed3157b
Evgeniya Stambolova <70650990+evgeniya-stambolova@users.noreply.github.com>
2020-10-05 19:40:07 +0300

    UPR

---
+++ Advanced/MergeTwoFiles.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class MergeTwoFiles {
+    public static void main(String[] args) throws IOException {
+
+        String pathOne = "C:\\Users\\Evgeniq.000\\Desktop\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\Exercises Resources\\inputOne.txt";
+        String pathTwo = "C:\\Users\\Evgeniq.000\\Desktop\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\Exercises Resources\\inputTwo.txt";
+        String outputPath = "outputMerge.txt";
+
+        PrintWriter writer = new PrintWriter(outputPath);
+        Files.readAllLines( Path.of(pathOne))
+                .forEach(writer::println);
+        Files.readAllLines(Path.of(pathTwo))
+                .forEach(writer::println);
+
+        writer.close();
+
+    }
+}
