dedcda240e242a19334a249b438270622a70ea22
Evgeniya Stambolova <70650990+evgeniya-stambolova@users.noreply.github.com>
2020-10-07 21:36:27 +0300

    Update MergeTwoFiles.java

--- Advanced/MergeTwoFiles.java
+++ Advanced/MergeTwoFiles.java
@@ -6,8 +6,8 @@ import java.nio.file.Path;
 public class MergeTwoFiles {
     public static void main(String[] args) throws IOException {
 
-        String pathOne = "C:\\Users\\Evgeniq.000\\Desktop\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\Exercises Resources\\inputOne.txt";
-        String pathTwo = "C:\\Users\\Evgeniq.000\\Desktop\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\Exercises Resources\\inputTwo.txt";
+        String pathOne = "C:\\Users\\000\\Desktop\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\Exercises Resources\\inputOne.txt";
+        String pathTwo = "C:\\Users\\000\\Desktop\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\Exercises Resources\\inputTwo.txt";
         String outputPath = "outputMerge.txt";
 
         PrintWriter writer = new PrintWriter(outputPath);
