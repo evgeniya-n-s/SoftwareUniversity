7e78597d0a6b00641abe71acd5ef3b86b0e29f9b
Evgeniya Stambolova <70650990+evgeniya-stambolova@users.noreply.github.com>
2020-10-12 23:29:43 +0300

    Update GetFolderSize.java

--- Advanced/GetFolderSize.java
+++ Advanced/GetFolderSize.java
@@ -3,7 +3,7 @@ import java.io.File;
 public class GetFolderSize {
     public static void main(String[] args) {
 
-        String path = "C:\\Users\\000\\Desktop\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\Exercises Resources";
+        String path = "Exercises Resources";
 
         File file = new File (path);
         long size = 0;
