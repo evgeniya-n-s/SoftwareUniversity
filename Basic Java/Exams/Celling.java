925a85a51565180b0d204ba4d98955ffc54adb90
Evgeniya Stambolova <70650990+evgeniya-stambolova@users.noreply.github.com>
2020-11-20 19:39:48 +0200

    UPR Basic

---
+++ Basic Java/Exams/Celling.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Celling {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double up = Math.ceil(23.45);
+        System.out.println(up);
+    }
+
+}
