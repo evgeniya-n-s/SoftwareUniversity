0844c8299d9fffa634ab7753a984915c17ba81fc
Evgeniya Stambolova <70650990+evgeniya-stambolova@users.noreply.github.com>
2020-11-21 14:06:10 +0200

    UPR Basic

---
+++ Basic Java/Exams/Division.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Division {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = Integer.parseInt(scanner.nextLine());
+        int b = Integer.parseInt(scanner.nextLine());
+        int result = a / b;
+        System.out.println(result);
+
+    }
+}
