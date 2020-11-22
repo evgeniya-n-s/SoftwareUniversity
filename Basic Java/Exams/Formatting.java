20672e2179bb7d0402da2bbdfefa339b122a268a
Evgeniya Stambolova <70650990+evgeniya-stambolova@users.noreply.github.com>
2020-11-22 14:03:59 +0200

    UPR Basic

---
+++ Basic Java/Exams/Formatting.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Formatting {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.printf("%.2f", 123.456);
+    }
+
+}
