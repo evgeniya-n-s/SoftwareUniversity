9857aba441e7d5b50377bb67894de8e8b1e4e437
Evgeniya Stambolova <70650990+evgeniya-stambolova@users.noreply.github.com>
2020-11-22 14:03:31 +0200

    UPR Basic

---
+++ Basic Java/Exams/Floor.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Floor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double down = Math.floor(23.45);
+        System.out.println(down);
+    }
+
+}
