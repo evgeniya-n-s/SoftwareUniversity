d0104c474dcc391660e48942da61390014e8d6bb
Evgeniya Stambolova <70650990+evgeniya-stambolova@users.noreply.github.com>
2020-11-25 19:50:01 +0200

    UPR Basic

---
+++ Basic Java/Exams/Plus.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Plus {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = Integer.parseInt(scanner.nextLine());
+        int b = Integer.parseInt(scanner.nextLine());
+        int result = a + b;
+        System.out.println(result);
+    }
+
+}
