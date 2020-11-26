6c47cf3af6d0532920866ae98a47ddaa69788593
Evgeniya Stambolova <70650990+evgeniya-stambolova@users.noreply.github.com>
2020-11-26 20:32:17 +0200

    UPR Basic

---
+++ Basic Java/Exams/TextNumberSecond.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class TextNumberSecond {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double a = 1.5;
+        double b = 2.5;
+        String sum = "The sum is: " + a + b;
+        System.out.println(sum);
+    }
+
+}
