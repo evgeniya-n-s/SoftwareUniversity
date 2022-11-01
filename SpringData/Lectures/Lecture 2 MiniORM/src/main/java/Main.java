760f32693d0ec7e566a434b5707f82e2cf3a6b98
Evgeniya Stambolova <evgeniya.stambolova@gmail.com>
2022-11-01 15:18:51 +0200

    Lecture 2 MiniORM
    ORM Fundamentals
    
    • Introduction to ORM;
    • Architecture;
    • Example;
    • Approaches;
    • ORM Advantages.

---
+++ SpringData/Lectures/Lecture 2 MiniORM/src/main/java/Main.java
@@ -0,0 +1,23 @@
+import entities.User;
+import orm.EntityManager;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+import static orm.MyConnector.createConnection;
+import static orm.MyConnector.getConnection;
+
+public class Main {
+
+    public static void main(String[] args) throws SQLException, IllegalAccessException {
+        createConnection("root", "", "");
+        Connection connection = getConnection();
+
+        EntityManager<User> userEntityManager = new EntityManager<>(connection);
+
+        User user = new User("pesho", 25, LocalDate.now());
+
+        userEntityManager.persist(user);
+    }
+}
