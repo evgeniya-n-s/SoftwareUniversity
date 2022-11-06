41306bd2af55c16aafdee152d54431e626e4ed24
Evgeniya Stambolova <evgeniya.stambolova@gmail.com>
2022-11-06 19:24:32 +0200

    Lecture 4 Hibernate Code First
    Hibernate Code First
    
    • Java Persistence API Inheritance;
    • Table Relations.

---
+++ SpringData/Lectures/Lecture 4 Hibernate Code First/src/main/java/Main.java
@@ -0,0 +1,54 @@
+import entitites.Bike;
+import entitites.Car;
+import entitites.Plane;
+import entitites.Vehicle;
+import hasEntities.Article;
+import hasEntities.PlateNumber;
+import hasEntities.Truck;
+import hasEntities.User;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("relations");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+
+        Vehicle car = new Car("Ford Focus", "Petrol", 5);
+        Vehicle bike = new Bike();
+        Vehicle plane = new Plane("Airbus", "Petrol", 200);
+
+        entityManager.persist(car);
+       entityManager.persist(bike);
+        entityManager.persist(plane);
+
+       PlateNumber number = new PlateNumber("123");
+        Truck truck1 = new Truck(number);
+       Truck truck2 = new Truck(number);
+
+       entityManager.persist(number);
+       entityManager.persist(truck1);
+       entityManager.persist(truck2);
+
+        Car fromDB = entityManager.find(Car.class, 1L);
+
+        System.out.println(fromDB.getSeats() + " " + fromDB.getModel());
+
+        Article article = new Article("alabala");
+        User author = new User("Toshko");
+
+        author.addArtice(article);
+        article.setAuthor(author);
+
+        entityManager.persist(author);
+
+        User user = entityManager.find(User.class, 2);
+
+        System.out.println();
+
+        entityManager.getTransaction().commit();
+        entityManager.close();
+    }
+}
---
+++ SpringData/Lectures/Lecture 4 Hibernate Code First/src/main/java/entitites/Bike.java
@@ -0,0 +1,15 @@
+package entitites;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+public class Bike extends Vehicle {
+    private static final String BIKE_TYPE = "BIKE";
+
+    public Bike() {
+        super(BIKE_TYPE);
+    }
+
+
+}
