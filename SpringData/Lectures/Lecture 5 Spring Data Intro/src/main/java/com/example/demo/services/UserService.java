aafa450b4b06922fd9cd1270211903cd2f7f8e4a
Evgeniya Stambolova <evgeniya.stambolova@gmail.com>
2022-11-08 11:38:34 +0200

    Lecture 5 Spring Data Intro
    Spring Data Intro
    
    • Spring Data Framework;
    • Spring Data Repositories;
    • Spring Data Query Creation;
    • Spring Data Services.

---
+++ SpringData/Lectures/Lecture 5 Spring Data Intro/src/main/java/com/example/demo/services/UserService.java
@@ -0,0 +1,8 @@
+package com.example.demo.services;
+
+import com.example.demo.models.User;
+
+public interface UserService {
+    void register(String username, int age);
+    User findByUsername(String username);
+}
